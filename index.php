ebd03b6986983909e09b7422a2c7a9f501f809fd
igor <work.gribov@gmail.com>
2021-02-14 23:49:46 +0300

    исправил ошибку не корректно указан адресс файлов

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if(!empty($type) && !empty($data)){
 
     if(!empty($foo['request'])){
 
-        $pathToFile = DOCUMENT_ROOT . '/' . $foo['request'] . '.json';
+        $pathToFile = DOCUMENT_ROOT . '/file/' . $foo['request'] . '.json';
 
         if (file_exists($pathToFile)) {
             $GetContentFile = file_get_contents($pathToFile);
